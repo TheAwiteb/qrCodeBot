c8c485cb0646a8d15ce233282fc0ceb8a9fad6f4
Awiteb <59842932+Awiteb@users.noreply.github.com>
2021-07-03 17:08:26 +0300

    add json_function.py

---
+++ json_function.py
@@ -0,0 +1,50 @@
+import json
+from config import JSON_FILE
+
+def get_data():
+    """ returns data from json file
+
+    Returns:
+        dict: dictionary of data
+    """
+    with open(JSON_FILE, 'r') as f:
+        return json.load(f)
+
+def get_key(key:str):
+    """ returns value of key
+
+    Args:
+        key (str): key you want value for
+
+    Returns:
+        any: value of key
+    """
+    return get_data().get(key)
+
+def push_data(new_data:dict):
+    """ change json file data to new_data
+
+    Args:
+        new_data (dict): new data
+    """
+    with open(JSON_FILE, 'w') as f:
+        json.dump(new_data, f, indent=4)
+
+def update_data(key:str, new_val:dict):
+    """ update key in json file
+
+    Args:
+        key (str): key you want update
+        new_val (dict): new value for key
+    """
+    data = get_data()
+    data[key] = new_val
+    push_data(new_data = data)
+
+def get_sum():
+    """ returns sum of values
+
+    Returns:
+        int: sum of values
+    """
+    return sum(get_data().values())
